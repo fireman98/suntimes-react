67ad33f660b804b62c105f10439f62dc0b5d8eec
fireman98 <bencebujnoczki@gmail.com>
2022-09-29 19:08:46 +0200

    TimesParameters

---
+++ src/components/TimesParameters.tsx
@@ -0,0 +1,58 @@
+import useComputed from "@/hooks/useComputed"
+import { SuntimesViewType } from "@/interfaces/Suntimes"
+import { DateTime } from "luxon"
+import { FunctionComponent, useMemo } from "react"
+import "./TimesParameters.scoped.scss"
+
+
+const TimesParameters: FunctionComponent<{
+    from: DateTime,
+    viewType: SuntimesViewType,
+    updateFrom: (payload: DateTime) => void,
+    updateViewType: (payload: SuntimesViewType) => void
+}> = ({ from, viewType, updateFrom, updateViewType }) => {
+
+    const SuntimesViewTypeValues = useMemo(() => Object.values(SuntimesViewType), [])
+
+
+    const fromAsISODate = useComputed({
+        get () {
+            return from.toISODate()
+        },
+
+        set (val) {
+            updateFrom(DateTime.fromISO(val).startOf('month'))
+        }
+    }, [from])
+
+    const goLeft = () => {
+        fromAsISODate.value = from.plus({ month: -1 }).toISODate()
+    }
+
+    const goRight = () => {
+        fromAsISODate.value = from.plus({ month: 1 }).toISODate()
+    }
+
+    return (
+        <div className="times-parameters">
+            <div className="mui-textfield date-input">
+                <input type="date" value={fromAsISODate.value} onChange={e => fromAsISODate.value = e.target.value} />
+                <button className="image-icon-wrapper time-selector__button" onClick={goLeft}>
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                <button className="image-icon-wrapper time-selector__button" onClick={goRight}>
+                    <i className="fas fa-chevron-right"></i>
+                </button>
+            </div>
+            <div className="mui-select">
+                <select value={viewType} onChange={e => updateViewType(e.target.value as SuntimesViewType)}>
+                    {SuntimesViewTypeValues.map(suntimesViewTypeValues => <option key={suntimesViewTypeValues} value={suntimesViewTypeValues}>
+                        {suntimesViewTypeValues}
+                    </option>)}
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default TimesParameters
--- src/views/TimesView.tsx
+++ src/views/TimesView.tsx
@@ -2,6 +2,7 @@ import { SuntimesViewType } from "@/interfaces/Suntimes"
 import { DateTime } from "luxon"
 import { useMemo, useState } from "react"
 import TimesTable from "@/components/TimesTable"
+import TimesParameters from "@/components/TimesParameters"
 
 function TimesView () {
     const [from, setFrom] = useState(DateTime.now().startOf("month"))
@@ -19,7 +20,7 @@ function TimesView () {
         <div className="view__times">
             <h1>Times - {monthName}</h1>
             <div className="view__times__times-table">
-
+                <TimesParameters from={from} updateFrom={(val) => setFrom(val)} viewType={viewType} updateViewType={(val) => setViewType(val)} />
                 <TimesTable from={from} to={to} viewType={viewType} />
             </div>
         </div>
